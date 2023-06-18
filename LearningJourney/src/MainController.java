cedc1f8bd7a87efeb7aec837da90a8ab47a6d18b
Rahima_Karimova <rkarimova17347@ada.edu.az>
2023-06-18 15:53:18 +0400

    calc 2

---
+++ LearningJourney/src/MainController.java
@@ -0,0 +1,41 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label result;
+    private double number1 =0;
+    private String operator ="";
+    private boolean start = true;
+    private Model model = new Model();
+   public void processNumbers(ActionEvent event) {
+    if (start) {
+        result.setText("");
+        start = false;
+    }
+    String value = ((Button) event.getSource()).getText();
+    result.setText(result.getText() + value);
+}
+
+    @FXML
+    public void processOperators(ActionEvent event){
+        String value = ((Button) event.getSource()).getText();
+        if(!value.equals("=")){
+            if(!operator.isEmpty())
+            return;
+            operator=value;
+            number1=Double.parseDouble(result.getText());
+            result.setText("");
+        }else{
+            if(operator.isEmpty())
+            return;
+            Double number2 = Double.parseDouble(result.getText());
+           double output = model.calculate(number1, number2, operator);
+           result.setText(String.valueOf(output));
+           operator="";
+           start=true;
+        }
+    }
+}
